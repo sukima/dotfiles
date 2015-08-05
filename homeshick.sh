6c8e40c116f0c1c3c96970f551c888581a64d257
Devin Weaver <suki@tritarget.org>
2015-08-05 00:43:28 -0400

    Rewrite of homeshick.sh install script

--- homeshick.sh
+++ homeshick.sh
@@ -1,61 +1,70 @@
 #!/bin/bash
 
-castles="sukima/dotfiles"
+tmpfilename="/tmp/${0##*/}.XXXXX"
 
-prompt_yesno() {
-  if [[ $2 = y ]]; then
-    def="[Yn]"
-  else
-    def="[yN]"
-  fi
-  read -p "$1 $def " answer
-  case "$answer" in
-    y|Y)
-      answer=y
-      ;;
-    n|N)
-      answer=n
-      ;;
-    *)
-      answer=$2
-      ;;
-  esac
-  [[ $answer = y ]]
-}
-
-prompt_castle() {
-  prompt_yesno "Include the castle $1?" $2
-}
-
-if prompt_yesno "Include plugin managers?" y; then
-  MANAGER=yes
+if type mktemp >/dev/null; then
+  tmpfile=$(mktemp $tmpfilename)
 else
-  MANAGER=no
+  tmpfile=$(echo $tmpfilename | sed "s/XX*/$RANDOM/")
 fi
 
-if prompt_castle vimrc y; then
-  castles="$castles sukima/vimrc"
-  if [[ $MANAGER = yes ]]; then
-    castles="$castles gmarik/Vundle.vim"
+trap 'rm -f "$tmpfile"' EXIT
+
+cat <<'EOF' > $tmpfile
+# Which Homeshick castles do you want to install?
+#
+# Each line is passed as the argument to `homeshick clone`
+#
+# Lines starting with '#' will be ignored, and an empty file aborts this
+# operation.
+#
+# If you remove a line that castle will NOT be installed.
+#
+# However, if you remove everything, the script will be aborted.
+
+# Plugin management
+gmarik/Vundle.vim
+tmux-plugins/tpm
+
+# Main castles
+sukima/dotfiles
+sukima/vimrc
+sukima/tmuxrc
+
+# Private castles
+#sukima/muttrc
+#sukirepoman@tritarget.org:securerc.git
+EOF
+
+${VISUAL:-vi} $tmpfile
+
+code=$?
+
+if [[ $code != 0 ]]; then
+  echo "Editor returned ${code}." 1>&2
+  exit 1
+fi
+
+castles=()
+
+while read line; do
+  castle=$(echo "$line" | sed '/^[ \t]*#/d;s/^[ \t]*\(.*\)[ \t]*$/\1/')
+  if [[ -n $castle ]]; then
+    castles+=("$castle")
   fi
+done <$tmpfile
+
+if [[ ${#castles[@]} -eq 0 ]]; then
+  echo "No castles to install. Aborting."
+  exit 0
 fi
 
-if prompt_castle tmuxrc y; then
-  castles="$castles sukima/tmuxrc"
-  if [[ $MANAGER = yes ]]; then
-    castles="$castles tmux-plugins/tpm"
-  fi
+if [[ ! -f $HOME/.homesick/repos/homeshick/homeshick.sh ]]; then
+  git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 fi
 
-if prompt_castle muttrc n; then
-  castles="$castles sukima/muttrc sukirepoman@tritarget.org:securerc.git"
-fi
-
-git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 source $HOME/.homesick/repos/homeshick/homeshick.sh
 
-for castle in $castles; do
-  homeshick clone $castle
+for castle in "${castles[@]}"; do
+  homeshick clone "$castle"
 done
-
-# vim: ts=2 sw=2 noet ft=sh fdm=marker
